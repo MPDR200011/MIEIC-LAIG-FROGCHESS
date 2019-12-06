af694136d72ef9daa4caea3e6a73c22cea81f290
Paulo Marques <paulodsam@gmail.com>
2019-12-06 11:53:11 +0000

    dunno what wrong with the board

--- webgl-src/gamePrimitives/Board.js
+++ webgl-src/gamePrimitives/Board.js
@@ -1,35 +1,27 @@
-class Board extends CGFobject{
-    constructor(scene){
+class Board extends CGFobject {
+    constructor(scene) {
         super(scene);
         this.piece = new BoardPiece(scene);
     }
 
 
 
-    updateScaleFactors(){
+    updateScaleFactors() {
         this.piece.updateScaleFactors();
     }
 
-    display(){
-        this.scene.pushMatrix();
-        this.scene.translate(2.5,0,2.5);
-        this.piece.display();
-        this.scene.popMatrix();
+    display() {
 
-        this.scene.pushMatrix();
-        this.scene.translate(2.5,0,-2.5);
-        this.piece.display();
-        this.scene.popMatrix();
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
 
-        this.scene.pushMatrix();
-        this.scene.translate(-2.5,0,-2.5);
-        this.piece.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.translate(-2.5,0,2.5);
-        this.piece.display();
-        this.scene.popMatrix();
+                this.scene.pushMatrix();
+                this.scene.translate(-18.5 +  5*i, 0, -18.5 + 5*j);
+                this.piece.display();
+                this.scene.popMatrix();
+            }
+        }
     }
+
 
 }
