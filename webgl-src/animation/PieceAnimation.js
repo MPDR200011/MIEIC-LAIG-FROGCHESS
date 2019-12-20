5160d0a396a18de59d03da43410ca02df2c466e5
Paulo Marques <paulodsam@gmail.com>
2019-12-20 10:04:55 +0000

    started working on animation and modifying the scene to handle it

---
+++ webgl-src/animation/PieceAnimation.js
@@ -0,0 +1,13 @@
+class PieceAnimation extends KeyframeAnimation{
+    constructor(scene,id,xi,zi,xf,zf){
+    super(scene,id);
+
+    for(let i = 0; i <2; i++){
+        this.addKeyframe(new Keyframe() )
+    }
+
+}
+
+
+
+}
