2c5e89544b02be996560efb9a46ffd48651b1d38
Paulo Marques <paulodsam@gmail.com>
2019-12-20 10:29:11 +0000

    animation for moving pieces

--- webgl-src/animation/PieceAnimation.js
+++ webgl-src/animation/PieceAnimation.js
@@ -1,9 +1,21 @@
 class PieceAnimation extends KeyframeAnimation{
-    constructor(scene,id,xi,zi,xf,zf){
+    constructor(scene,id,xi,zi,xf,zf,steps,maxY,time){
     super(scene,id);
+        this.id = id;
+        let xInc = (xf-xi )/ steps
+        let yInc = floor(maxY/(steps/2))
+        let zInc = (zf-zi)/steps
+        let timeStep = time/steps
 
-    for(let i = 0; i <2; i++){
-        this.addKeyframe(new Keyframe() )
+    for(let i = 0; i <steps; i++){
+
+        if(i > steps/2 ){
+            yInc*= -1
+        }
+        let translation = [xInc*i,yInc*i,zInc *i]
+        
+        this.addKeyframe(new Keyframe(timeStep* i,translation,[],[] ))
+
     }
 
 }
